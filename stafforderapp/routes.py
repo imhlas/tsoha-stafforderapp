9a39c3230a128485f89025e1cb03ba6671191ecb
Iida Lassila <iida.m.lassila@helsinki.fi>
2023-09-10 15:07:45 +0300

    Implement login and registration routes and templates

--- stafforderapp/routes.py
+++ stafforderapp/routes.py
@@ -1,6 +1,26 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, session
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        # check username and password
+        session["username"] = username
+        return redirect("/")
+
+@app.route("/create_user", methods=["GET"])
+def create_user():
+    return render_template("create_user.html")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
