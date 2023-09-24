52922c203c6643ff5a3d23ece6325cefb9bfcd42
Iida Lassila <iida.m.lassila@helsinki.fi>
2023-09-24 12:16:49 +0300

    Tables for users, products & images

--- stafforderapp/schema.sql
+++ stafforderapp/schema.sql
@@ -8,5 +8,12 @@ CREATE TABLE users (
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     name TEXT,
-    price DECIMAL(10, 2)
+    price DECIMAL(10, 2),
+    product_number INTEGER 
+);
+
+CREATE TABLE images (
+    id SERIAL PRIMARY KEY,
+    product_number INTEGER,
+    data BYTEA
 );
